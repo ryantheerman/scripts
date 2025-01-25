a60cc5b68a187887c18f9872ebbd196fdd54a178
ryan theerman <ryantheerman@proton.me>
2025-01-24 19:33:50 -0500

    hack scripts to reapply x configs after system update breaks them

---
+++ xfix.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# this script is a bit of a hack.
+# for some reason I'm not yet sure of, my x configs sometimes get borked after a system update. something to do with pacman hooks??? gotta look into it.
+# but for now, this script resets my x configs which are typically initialized in .xinitrc.
+# xinit has some other calls which are not affected by the system update issue, so they're not included here (redshift & and picom & for instance).
+# there's probably a better way to set some of these configs, but it needs research. don't have the time right now (but I will in future, right? right?), so here's this hack.
+# i'll hook it to the end of pacman -Syu or something.
+# but maybe I shouldn't actually. the update doesn't ALWAYS cause a problem. do i run into trouble running some of these settings on top of each other?
+# i think maybe just doubling/tripling etc the xcape settings. will test.
+
+setxkbmap us -v dvorak
+xset r rate 200 30
+xset s 1800 1800 +dpms
+xmodmap $HOME/.Xmodmap
+xcape -e 'Hyper_R=Escape'
+
+# below xcape config seems unaffected by the system update issue. running it twice (at xinit and after update) seems to cause trouble)
+#xcape -e 'Control_L=Control_L|a'
+xinput set-button-map 11 1 1 3 4 5 6 7
+xrdb $HOME/.Xresources
+
+
+# okay yes, disabling the xcape left control setting lets me run this scripts with no unexpected behavior. it's idempotent but for that line.
