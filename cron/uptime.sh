e58298bc82597bc341e4b3ede8a704ecf8c3f178
ryan theerman <ryantheerman@proton.me>
2025-01-29 23:02:56 -0500

    fixed bug in uptime.sh

--- cron/uptime.sh
+++ cron/uptime.sh
@@ -7,13 +7,12 @@ hours=$(echo "$uptime" | grep -oP '\d+(?= hour)')
 minutes=$(echo "$uptime" | grep -oP '\d+(?= minute)')
 
 formatted_uptime=""
-if [ -n "$weeks" ]; then
-  weeksdays=$(($weeks*7))
-  if [ -n "$days" ]; then
-      formatted_uptime=" $(($weeksdays+$days))d"
-  fi
-elif [ -n "$days" ]; then
-    formatted_uptime="${formatted_uptime} ${days}d"
+if [ -n "$weeks" ] && [ -n "$days" ]; then
+  formatted_uptime=" $(($weeks*7+$days))d"
+  elif [ -n "$days" ]; then
+      formatted_uptime=" ${days}d"
+  elif [ -n "$weeks" ]; then
+      formatted_uptime=" $(($weeks*7))d"
 fi
 if [ -n "$hours" ]; then
   formatted_uptime="${formatted_uptime} ${hours}h"
