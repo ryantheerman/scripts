0e43b8ccdd476ffaae1864af3b50e0c5dcc891a1
ryan theerman <ryantheerman@proton.me>
2025-01-29 22:38:03 -0500

    modified uptime.sh to convert weeks into days in output

--- cron/uptime.sh
+++ cron/uptime.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 uptime=$(uptime -p)
+weeks=$(echo "$uptime" | grep -oP '\d+(?= week)')
 days=$(echo "$uptime" | grep -oP '\d+(?= day)')
 hours=$(echo "$uptime" | grep -oP '\d+(?= hour)')
 minutes=$(echo "$uptime" | grep -oP '\d+(?= minute)')
 
 formatted_uptime=""
-if [ -n "$days" ]; then
-  formatted_uptime=" ${days}d"
+if [ -n "$weeks" ]; then
+  weeksdays=$(($weeks*7))
+  if [ -n "$days" ]; then
+      formatted_uptime=" $(($weeksdays+$days))d"
+  fi
+elif [ -n "$days" ]; then
+    formatted_uptime="${formatted_uptime} ${days}d"
 fi
 if [ -n "$hours" ]; then
   formatted_uptime="${formatted_uptime} ${hours}h"
