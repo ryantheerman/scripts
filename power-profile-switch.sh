6a79980771c344f27e68554f6b323f0149ba18cc
ryan theerman <ryantheerman@proton.me>
2025-02-22 23:58:05 -0500

    removed the performance conditions. superfluous.

--- power-profile-switch.sh
+++ power-profile-switch.sh
@@ -6,7 +6,7 @@ echo "current profile: $current_profile"
 # if we just plugged in
 # and we're in BAT power-saving mode -> switch to AC power-saving mode
 # or we're in BAT balanced mode -> switch to AC balanced mode
-# or we're in performance mode -> do nothing
+# or we're in performance mode (or any other mode) -> do nothing
 # in any case, write the profile to the i3status data file
 if [ $1 == "true" ]; then
 #    echo "we're plugged in"
@@ -16,13 +16,11 @@ if [ $1 == "true" ]; then
     elif [ $current_profile == "balanced-battery" ]; then
         tuned-adm profile balanced
         echo "balanced (ac)" > /home/match/.config/i3status/power-profile
-    elif [ $current_profile == "throughput-performance" ]; then
-        echo "performance" > /home/match/.config/i3status/power-profile
     fi  
 # else if we just unplugged
 # and we're in AC power saving-mode -> switch to BAT power-saving mode
 # or we're in AC balanced mode -> switch to BAT balanced mode
-# or we're in performance mode -> do nothing
+# or we're in performance mode (or any other mode) -> do nothing
 # in any case, write the profile to the i3status data file
 elif [ $1 == "false" ]; then
 #    echo "we're unplugged"
@@ -32,7 +30,5 @@ elif [ $1 == "false" ]; then
     elif [ $current_profile == "balanced" ]; then
         tuned-adm profile balanced-battery
         echo "balanced (bat)" > /home/match/.config/i3status/power-profile
-    elif [ $current_profile == "throughput-performance" ]; then
-        echo "performance" > /home/match/.config/i3status/power-profile
     fi
 fi
