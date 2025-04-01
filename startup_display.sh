ff83f8040a54e55306e7d208f4a516ad329e9298
ryan theerman <ryantheerman@proton.me>
2025-04-01 16:19:11 -0400

    tweaked display script

--- startup_display.sh
+++ startup_display.sh
@@ -2,7 +2,7 @@
 
 # script to start up monitors
 
-if [[ $(xrandr --query | grep "DP-2-6-8 connected") ]]; then
+if [[ $(xrandr --query | grep "DP-1-6-8 connected") ]]; then
     echo "multi monitor"
     # check if required modes exist
     if [[ ! $(xrandr --query | grep "2560x1440_30.00") ]]; then
@@ -12,22 +12,22 @@ if [[ $(xrandr --query | grep "DP-2-6-8 connected") ]]; then
 
         # add the modes
         xrandr --addmode eDP-1 1920x1080_60.00
-        xrandr --addmode DP-2-6-8 2560x1440_30.00
-        xrandr --addmode DP-2-6-1-8 2560x1440_30.00
+        xrandr --addmode DP-1-6-8 2560x1440_30.00
+        xrandr --addmode DP-1-6-1-8 2560x1440_30.00
     fi
     
     # whether newly created or not, apply the display config with the custom modes
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1613x1440 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off --output HDMI-3 --off --output DP-2-5 --off --output DP-2-6 --off --output DP-1-6-8 --mode 2560x1440_30.00 --pos 0x0 --rotate normal --output DP-2-6-1 --off --output DP-1-6-1-8 --mode 2560x1440_30.00 --pos 2560x0 --rotate normal
 
 
-    #xrandr --output eDP-1 --mode 1920x1080_60.00 --output DP-2-6-8 --mode 2560x1440_30.00 --above eDP-1 --output DP-2-6-1-8 --mode 2560x1440_30.00 --right-of DP-2-6-8
+    #xrandr --output eDP-1 --mode 1920x1080_60.00 --output DP-1-6-8 --mode 2560x1440_30.00 --above eDP-1 --output DP-1-6-1-8 --mode 2560x1440_30.00 --right-of DP-1-6-8
     
 else
     echo "single monitor"
     xrandr --output eDP-1 --mode 1920x1080 --primary
-    xrandr --output DP-2-6-8 --off
-    xrandr --output DP-2-6-1-8 --off
+    xrandr --output DP-1-6-8 --off
+    xrandr --output DP-1-6-1-8 --off
 fi
 
 # update the background scaling
-feh --bg-fill $HOME/pictures/backgrounds/G5qiJ87.jpg
+feh --bg-fill $HOME/pictures/backgrounds/puget_sound.png.jpg
