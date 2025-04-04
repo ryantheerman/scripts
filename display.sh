aba6d821c91de5f4784f563d90d58d8fd847452b
ryan theerman <ryantheerman@proton.me>
2025-04-04 08:31:02 -0400

    corrected output query for ganymede integrated docked scenario

--- display.sh
+++ display.sh
@@ -2,7 +2,7 @@
 
 if [[ $($HOME/scripts/readMoon.sh) == "ganymede" ]]; then
     if [[ $(glxinfo | grep "renderer string: Mesa Intel(R)") ]]; then
-        if [[ $(xrandr --query | grep "DP-1-6-8 connected") ]]; then
+        if [[ $(xrandr --query | grep "DP-3-6-8 connected") ]]; then
             ~/.screenlayout/ganymede-docked-integrated.sh
         else
             ~/.screenlayout/ganymede-onboard-integrated.sh
