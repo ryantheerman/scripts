b4fcd8f4fcbdd2507505d8182010999a4efd898b
ryan theerman <ryantheerman@proton.me>
2025-09-30 21:13:29 -0400

    fixed battery script

--- batt.sh
+++ batt.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-acpi | grep 'Battery 0' | awk '{print $4}' | sed 's|,||g'
+batteryLine=$(acpi | grep 'Battery 0')
+batteryLineFourthElement=$(acpi | grep 'Battery 0' | awk '{print $4}' |  sed 's|,||g')
+#echo $batteryLineFourthElement
+
+if [[ $batteryLineFourthElement == "charging" ]] ; then
+	echo $batteryLine | awk '{print $5}' | sed 's|,||g'
+else
+	echo $batteryLine | awk '{print $4}' | sed 's|,||g'
+fi
