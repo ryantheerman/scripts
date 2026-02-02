f6f5ae00296acd19ac664c819ad3fb449001d19a
ryan theerman <ryantheerman@proton.me>
2026-02-01 20:59:08 -0500

    wrapper script to spin up 2 animated wallpapers (for use with niri)

---
+++ mpvpaper-wrapper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mpvpaper -o "no-audio --panscan=1.0" -n 1200 ALL $LIVE_WALLPAPER_WORKSPACE &
+mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 ALL $LIVE_WALLPAPER_BACKDROP &
+
+
+
+
+
+
+# test
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 eDP-1 $PAPER_1 &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 eDP-1 $PAPER_2 &
+
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 DP-9 $PAPER_3 &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 DP-9 $PAPER_4 &
+
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 DP-7 $PAPER_5 & 
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 DP-7 $PAPER_6 &
+
+
+# with audio
+#mpvpaper -o --panscan=1.0 -n 460 ALL $LIVE_WALLPAPER_WORKSPACE &
+#mpvpaper-backdrop -o --panscan=1.0 -n 422 ALL $LIVE_WALLPAPER_BACKDROP &
