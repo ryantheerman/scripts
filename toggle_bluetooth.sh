0f528b175903e54de19a0d83ff7594ad40151ba4
ryan theerman <ryantheerman@proton.me>
2026-02-01 20:54:52 -0500

    shortened bluetooth indicator (x sessions)

--- toggle_bluetooth.sh
+++ toggle_bluetooth.sh
@@ -2,9 +2,9 @@
 
 if [[ $(rfkill | grep "tpacpi_bluetooth_sw unblocked unblocked") || $(rfkill | grep "hci0           unblocked unblocked") ]]; then
     rfkill block 0
-    echo "<span color='red'>bluetooth</span>" > /home/match/.config/i3status/bluetooth
+    echo "<span color='red'>bt</span>" > /home/match/.config/i3status/bluetooth
 else
     rfkill unblock 0
-    echo "bluetooth" > /home/match/.config/i3status/bluetooth
+    echo "bt" > /home/match/.config/i3status/bluetooth
 fi
 killall -USR1 i3status
