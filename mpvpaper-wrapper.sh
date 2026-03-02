7eaa589710040e6e1dcd34cd4d4bc611db2feda0
ryan theerman <ryantheerman@proton.me>
2026-03-02 12:25:56 -0500

    now always gets the correct output name according to niri for use in sending different mpvpaper streams to distinct outputs

--- mpvpaper-wrapper.sh
+++ mpvpaper-wrapper.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+# outputs ##
+left_32="$(niri msg outputs | grep 'Dell Inc. DELL U3223QE 8M79K04' | awk '{print $7}' | tr -d '()')"
+right_32="$(niri msg outputs | grep 'Dell Inc. DELL U3223QE 6F129P3' | awk '{print $7}' | tr -d '()')"
+right_27="$(niri msg outputs | grep 'Dell Inc. DELL U2723QE 1SS19P3' | awk '{print $7}' | tr -d '()')"
+
+echo $left_32
+echo $right_32
+echo $right_27
+
 mpvpaper -o "no-audio --panscan=1.0" -n 1200 ALL $LIVE_WALLPAPER_WORKSPACE &
 mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 ALL $LIVE_WALLPAPER_BACKDROP &
 
-#mpvpaper -o "no-audio --panscan=1.0" -n 1200 ALL $PAPER_1 &
-#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 ALL $PAPER_2 &
 
+# distinct outputs
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 eDP-1 $LIVE_WALLPAPER_WORKSPACE &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 eDP-1 $LIVE_WALLPAPER_WORKSPACE &
 
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 $left_32 $LIVE_WALLPAPER_BACKDROP &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 $left_32 $LIVE_WALLPAPER_BACKDROP &
 
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 $right_32 $LIVE_WALLPAPER_WORKSPACE &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 $right_32 $LIVE_WALLPAPER_WORKSPACE &
 
-
-# test
-#mpvpaper -o "no-audio --panscan=1.0" -n 1200 eDP-1 $PAPER_1 &
-#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 eDP-1 $PAPER_2 &
-
-#mpvpaper -o "no-audio --panscan=1.0" -n 1200 DP-9 $PAPER_3 &
-#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 DP-9 $PAPER_4 &
-
-#mpvpaper -o "no-audio --panscan=1.0" -n 1200 DP-7 $PAPER_5 & 
-#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 DP-7 $PAPER_6 &
-
-
-# with audio
-#mpvpaper -o --panscan=1.0 -n 460 ALL $LIVE_WALLPAPER_WORKSPACE &
-#mpvpaper-backdrop -o --panscan=1.0 -n 422 ALL $LIVE_WALLPAPER_BACKDROP &
+#mpvpaper -o "no-audio --panscan=1.0" -n 1200 $right_27 $LIVE_WALLPAPER_BACKDROP &
+#mpvpaper-backdrop -o "no-audio --panscan=1.0" -n 1200 $right_27 $LIVE_WALLPAPER_BACKDROP &
