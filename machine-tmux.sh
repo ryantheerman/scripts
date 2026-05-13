d4bebc190d1ed1ac1bbadec10188cf23f309fac6
ryan theerman <ryantheerman@proton.me>
2026-05-13 08:44:35 -0400

    removed comments

--- machine-tmux.sh
+++ machine-tmux.sh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-#cd ~
-#
-#connected_wireless=$(nmcli | grep "connected to diaper-butt-home" | awk '{print $4}')
-#connected_wired=$(nmcli | grep "inet4 192.168.30.")
-#
-#target_net=$(cat $HOME/scripts/.netdef)
-
-
-# check if we need to use the tailnet
-
 hostname=$1
 
 if ! bash on-home-network.sh $hostname && [[ $hostname != 'piratebox' ]] && [[ $hostname != 'claudebox-server' ]] && [[ $hostname != 'brocade' ]]; then
